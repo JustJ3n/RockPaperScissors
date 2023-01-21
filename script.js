6878f2a33aaedf46bfec65c10132be6f4ff968d2
Jenny Lee <jlee172754@gmail.com>
2023-01-20 20:49:10 -0500

    create a function that plays a single round of the game and return the round result

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ function getComputerChoice() {
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'paper'){
+        return `You Lose! Paper beats Rock`;
+    } else if (
+        playerSelection == 'paper' && computerSelection == 'rock'){
+            return `You Win! Paper beats Rock`;
+    } else if (
+        playerSelection == 'rock' && computerSelection == 'scissors'){
+            return `You Win! Rock beats Scissors`;
+    } else if (
+        playerSelection == 'scissors' && computerSelection == 'rock'){
+            return `You Lose! Rock beats Scissors`;
+     } else if (
+        playerSelection == 'paper' && computerSelection == 'scissors'){
+            return `You Lose! Scissors beats Paper`; 
+    } else if (
+        playerSelection == 'scissors' && computerSelection == 'paper'){
+            return `You Win! Scissors beats Paper`;
+    } else {
+        return `It's a tie!`;
+    }
+}
