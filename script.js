a307d5b552d3d889e7b976173591f4e227792bd8
Jenny Lee <jlee172754@gmail.com>
2023-01-23 11:33:51 -0500

    define variables to access the DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const userScore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result")
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -30,5 +40,3 @@ function playRound(playerSelection, computerSelection) {
         return `It's a tie!`;
     }
 }
-
-console.log(playRound(playerSelection, computerSelection));
